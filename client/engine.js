15add03b5aa7cab30549a0065b8a759d8bdd1307
Ross Hadden <rosshadden@gmail.com>
2012-04-05 22:40:59 -0400

    Basic module setup.

--- client/engine.js
+++ client/engine.js
@@ -1,1 +1,40 @@
-console.log('test2');
+define(['engine/utilities'], function(utilities){
+	var Engine = function(options){
+		var self = this,
+		
+		execute = function(){
+			if(typeof options[what] === 'function'){
+				options[what].call(self);
+			}
+		},
+		
+		init = (function(){
+			execute('init');
+		})(),
+		
+		update = function(){
+			execute('update');
+		},
+		
+		paint = function(){
+			execute('paint');
+		},
+		
+		main = function(){
+			requestAnimFrame(main);
+			
+			update();
+			paint();
+		},
+		
+		start = function(){
+			main();
+		};
+		
+		return {
+			start:	start
+		};
+	};
+	
+	return Engine;
+});
