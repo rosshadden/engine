805ed57eb386263ad3319cbd92f6bea9ad3033a2
Ross Hadden <rosshadden@gmail.com>
2012-04-06 13:10:32 -0400

    Added more utilities.

--- client/utilities.js
+++ client/utilities.js
@@ -1,16 +1,49 @@
 define(function(utilities){
-	requestAnimFrame = (function(){
-	  return  window.requestAnimationFrame       || 
-		      window.webkitRequestAnimationFrame || 
-		      window.mozRequestAnimationFrame    || 
-		      window.oRequestAnimationFrame      || 
-		      window.msRequestAnimationFrame     || 
-		      function( callback ){
-		        window.setTimeout(callback, 1000 / 60);
-		      };
-	})();
-	
 	return {
-		requestAnimFrame:	requestAnimFrame
+		requestAnimFrame:	(function(){
+			return	window.requestAnimationFrame       || 
+					window.webkitRequestAnimationFrame || 
+					window.mozRequestAnimationFrame    || 
+					window.oRequestAnimationFrame      || 
+					window.msRequestAnimationFrame     || 
+					function(callback){
+						window.setTimeout(callback, 1000 / 60);
+					};
+		})(),
+		
+        inherit:	(function(){
+            var Temp = function(){};
+            
+            return function(Child, Parent){
+                Temp.prototype = Parent.prototype;
+                Child.prototype = new Temp();
+                Child.uber = Parent.prototype;
+                Child.prototype.constructor = Child;
+                
+				return Child;
+            };
+        })(),
+        
+		extend:	function(source, target){
+			var object = {};
+			
+			for(key in source){
+				object[key] = source[key];
+			}
+			
+			for(key in target){
+				object[key] = target[key];
+			}
+			
+			object.parent = source;
+			
+			return object;
+		},
+			
+		listen:	function(context){
+			return function(){
+				$(context).on.apply($(context), arguments);
+			};
+		}
 	};
 });
