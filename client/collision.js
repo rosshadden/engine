f4a29970003d82c10f0e01c9336f10f7a3be519a
Ross Hadden <rosshadden@gmail.com>
2012-04-09 04:15:06 -0400

    Added collision detection.

--- client/collision.js
+++ client/collision.js
@@ -1,16 +1,65 @@
-define(function(){
-	//	Need to make face-collision vs area-collision.
-	return function(entityOne, entityTwo) {
-		var	left1 = entityOne.position.x,
-			right1 = entityOne.position.x + entityOne.dim.width,
-			top1 = entityOne.position.y,
-			bottom1 = entityOne.position.y + entityOne.dim.height,
-			
-			left2 = entityTwo.position.x,
-			right2 = entityTwo.position.x + entityTwo.dim.width,
-			top2 = entityTwo.position.y,
-			bottom2 = entityTwo.position.y + entityTwo.dim.height;
+define(['engine/world', 'engine/utilities'], function(world, utilities){
+	var self = this,
+	
+	rectangle = function(one, two){
+		var map = function(entity){
+			return {
+				x:		entity.position.x,
+				y:		entity.position.y,
+				width:	entity.dim.width,
+				height:	entity.dim.height
+			};
+		};
 		
-		return !(left1 > right2 || left2 > right1 || top1 > bottom2 || top2 > bottom1);
+		one = map(one);
+		two = map(two);
+		
+		one.left = one.x;
+		one.right = one.x + one.width;
+		one.top = one.y;
+		one.bottom = one.y + one.height;
+		
+		two.left = two.x;
+		two.right = two.x + two.width;
+		two.top = two.y;
+		two.bottom = two.y + two.height;
+		
+		if(
+			one.bottom < two.top
+		||	one.top > two.bottom
+		||	one.right < two.left
+		||	one.left > two.right
+		){
+			return false;
+		}
+		
+		return true;
+	},
+	
+	wall = function(entity, which){
+		var map = world.maps.get(world.maps.getCurrent());
+		
+		if((which === 'left' || which === 'any') && entity.position.x <= 0){
+			return true;
+		}
+		
+		if((which === 'right' || which === 'any') && entity.position.x >= map.element.width){
+			return true;
+		}
+		
+		if((which === 'top' || which === 'any') && entity.position.y <= 0){
+			return true;
+		}
+		
+		if((which === 'bottom' || which === 'any') && entity.position.y >= map.element.height){
+			return true;
+		}
+		
+		return false;
+	};
+	
+	return {
+		rectangle:	rectangle,
+		wall:		wall
 	};
 });
