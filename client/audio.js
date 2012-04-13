46f6f0b346c418725bbc6385fa104b53dc5103dd
Ross Hadden <rosshadden@gmail.com>
2012-04-13 01:05:33 -0400

    Got audio API working!

--- client/audio.js
+++ client/audio.js
@@ -1,34 +1,38 @@
 define(['engine/resources'], function(resources){
 	var self = this;
 
-	var buffer,
+	var buffers = {},
 		context = new webkitAudioContext();
 
-	var load = function(url){
-		var request = new XMLHttpRequest();
+	var load = function(name, url){
+		var def = new $.Deferred,
+			request = new XMLHttpRequest();
 
 		request.open('GET', url, true);
 		request.responseType = 'arraybuffer';
 
 		request.onload = function(){
 			context.decodeAudioData(request.response, function(soundBuffer){
-				buffer = soundBuffer;
+				buffers[name] = soundBuffer;
 			});
+			
+			def.resolve(buffers[name]);
 		};
 
 		request.send();
+		
+		return def.promise();
 	};
 
-	var play = function(){
+	var play = function(name){
 		var source = context.createBufferSource();
 
-		source.buffer = buffer;
+		source.buffer = buffers[name];
 		source.connect(context.destination);
 		source.noteOn(0);
 	};
 	
 	return {
-		context:	context,
 		load:		load,
 		play:		play
 	};
