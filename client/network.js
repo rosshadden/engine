49a0da49814589df8c7423838d8dfb759f9b2d2c
Ross Hadden <rosshadden@gmail.com>
2012-04-14 14:43:11 -0400

    Added network.emit() and network.on() (wrapping socket.io's functions).

--- client/network.js
+++ client/network.js
@@ -4,9 +4,23 @@ define(function(){
 
 	connect = function(){
 		socket = io.connect('http://localhost');
+	},
+
+	on = function(){
+		var args = Array.prototype.slice.call(arguments);
+		
+		socket.on.apply(socket, args);
+	},
+
+	emit = function(){
+		var args = Array.prototype.slice.call(arguments);
+		
+		socket.emit.apply(socket, args);
 	};
 
 	return {
-		connect:	connect
+		connect:connect,
+		on:		on,
+		emit:	emit
 	};
 });
