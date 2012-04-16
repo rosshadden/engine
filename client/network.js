090f7a2dbf10da9d3af137c7fa066f58ee3f3e84
Ross Hadden <rosshadden@gmail.com>
2012-04-15 21:32:44 -0400

    Some work on network module.

--- client/network.js
+++ client/network.js
@@ -1,26 +1,38 @@
 define(function(){
-	var self = this,
-		socket,
+	var network = (function(){
+		var self = this,
+			socket,
 
-	connect = function(){
-		socket = io.connect('http://localhost');
-	},
-
-	on = function(){
-		var args = Array.prototype.slice.call(arguments);
+		connect = function(){
+			socket = io.connect('http://localhost');
 		
-		socket.on.apply(socket, args);
-	},
+			return self.methodss;
+		},
 
-	emit = function(){
-		var args = Array.prototype.slice.call(arguments);
+		on = function(){
+			var args = Array.prototype.slice.call(arguments);
 		
-		socket.emit.apply(socket, args);
-	};
+			socket.on.apply(socket, args);
+		
+			return self.methodss;
+		},
 
-	return {
-		connect:connect,
-		on:		on,
-		emit:	emit
-	};
+		emit = function(){
+			var args = Array.prototype.slice.call(arguments);
+		
+			socket.emit.apply(socket, args);
+		
+			return self.methodss;
+		};
+		
+		self.methodss = {
+			connect:connect,
+			on:		on,
+			emit:	emit
+		};
+	
+		return self.methodss;
+	})();
+	
+	return network;
 });
