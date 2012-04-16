0146c8d5fccc2de2347bc737da5b851dbdc5bd69
Ross Hadden <rosshadden@gmail.com>
2012-04-16 01:06:57 -0400

    Attempt at encapsulating socket.io.

--- server/engine.js
+++ server/engine.js
@@ -1,6 +1,6 @@
 var engine = function(app){
 	var router = require('./router'),
-		network = require('./network');
+		network = require('./network')(app);
 	
 	for(var route in router){
 		app.get(route, router[route]);
--- server/network.js
+++ server/network.js
@@ -1,7 +1,22 @@
-var network = function(socket){
-	socket.on('testing', function(){
-		console.log('testing');
+var network = function(app){
+	var	io = require('socket.io').listen(app),
+	
+	players = {},
+	numPlayers = 0;
+	
+	io.sockets.on('connection', function(socket){
+		players[socket.id] = {
+			socket:	socket
+		};
+		
+		console.log('Player #%d connected.', ++numPlayers);
 	});
+	
+	return {
+		io:			io,
+		players:	players,
+		numPlayers:	numPlayers
+	};
 };
 
 module.exports = network;
