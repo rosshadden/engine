f009b077835ba098e3a2976ea3d6e8ea930e7b7c
Ross Hadden <rosshadden@gmail.com>
2012-04-18 00:58:06 -0400

    Removed /GET path.

--- server/router.js
+++ server/router.js
@@ -3,23 +3,6 @@ module.exports = {
 		next();
 	},
 	
-	'/GET':	function(request, response){
-		var data = '',
-			url = parseURL(request.url, true);
-	
-		response.contentType('application/json');
-	
-		switch(url.query.for){
-			case 'debug':
-				data = 'debug';
-				break;
-			default:
-				data = 'default';
-		}
-	
-		response.json(data);
-	},
-	
 	'/maps/:path':	function(request, response){
 		try{
 			var map = require('../../resources/maps/' + request.params.path + '.json');
