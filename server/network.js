b16e37d85d37becfb27eaa90ab7617c28f2690e0
Ross Hadden <rosshadden@gmail.com>
2012-04-20 20:07:54 -0400

    Got socket event binding working.

--- server/network.js
+++ server/network.js
@@ -1,32 +1,53 @@
 var network = function(app){
 	app.io = app.io || require('socket.io').listen(app),
+	emitter = new (require('events').EventEmitter),
+	q = require('q'),
 	
+	events = {},
 	players = {},
 	numPlayers = 0;
 	
-	var start = function(handler){
-		app.io.sockets.on('connection', function(socket){
-			console.log(socket.id, players);
-			if(!(socket.id in players)){
-				players[socket.id] = {
-					id:		socket.id,
-					socket:	socket
-				};
-				
-				console.log('Player #%d connected.', ++numPlayers);
-			}
+	app.io.sockets.on('connection', function(socket){
+		/*if(!(socket.id in players)){
+			players[socket.id] = {
+				id:		socket.id,
+				socket:	socket
+			};
 			
-			if(typeof handler === 'function'){
-				handler.call(this, socket);
-			}
+			console.log('Player #%d connected.', ++numPlayers);
+		}*/
+		
+		emitter.on('bind', function(event, handler){
+			socket.on(event, handler);
 		});
+		
+		emitter.on('trigger', function(event, data){
+			socket.emit(event, data);
+		});
+		
+		for(var event in events){
+			socket.on(event, events[event]);
+		}
+	});
+	
+	var on = function(event, handler){
+		if(['bind', 'trigger'].indexOf(event) > -1){
+			throw new Error("The event '" + event + "' is reserved by the engine.network module.");
+		}
+		
+		events[event] = handler;
+		emitter.emit('bind', event, handler);
+	},
+	
+	emit = function(event, data){
+		emitter.emit('trigger', event, data);
 	};
 	
 	return {
-		io:			app.io,
 		players:	players,
 		numPlayers:	numPlayers,
-		start:		start
+		on:			on,
+		emit:		emit
 	};
 };
 
