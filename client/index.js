c315667a7cd5387e697fbee5a8c53a4ece535f00
Ross Hadden <rosshadden@gmail.com>
2012-05-07 14:48:10 -0400

    Added storage module.

--- client/index.js
+++ client/index.js
@@ -4,13 +4,15 @@ define([
 	'./display/index',
 	'./input/index',
 	'./media/index',
+	'./storage/index',
 	'./utilities/index',
 	'./lib/index'
-], function(engine, cycle, display, input, media, utilities, lib){
+], function(engine, cycle, display, input, media, storage, utilities, lib){
 	cycle(engine);
 	display(engine);
 	input(engine);
 	media(engine);
+	storage(engine);
 	engine.utilities = utilities(engine);
 	
 	return engine;
