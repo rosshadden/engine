0bec204ab599eccf78d1a662880c899e8585dde0
Ross Hadden <rosshadden@gmail.com>
2012-05-10 15:45:08 -0400

    Namespaced network functions.

--- client/network/net.js
+++ client/network/net.js
@@ -1,12 +1,8 @@
 define(['/socket.io/socket.io.js'], function(IO){
 	return function(engine){
 		engine.c('net')
-		.defines({
-			connect:function(path){
-				this.socket = io.connect(path || '');
-			},
-			
-			bind:		function(){
+		.namespaces({
+			on:		function(){
 				var args = [].slice.call(arguments);
 				
 				this.socket.on.apply(this.socket, args);
@@ -20,6 +16,23 @@ define(['/socket.io/socket.io.js'], function(IO){
 				this.socket.emit.apply(this.socket, args);
 				
 				return this;
+			}
+		})
+		.defines({
+			connect:function(path){
+				this.socket = io.connect(path || '');
+			},
+			
+			bind:		function(){
+				var args = [].slice.call(arguments);
+				
+				return this.net_on.apply(this, args);
+			},
+			
+			emit:	function(){
+				var args = [].slice.call(arguments);
+				
+				return this.net_emit.apply(this, args);
 			}
 		})
 		.init(function(){
